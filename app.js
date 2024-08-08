f3f025202543b9ecd02cfa2881338c5115827a9e
K-D-Piper <kirsten.piper@mscoding.org>
2024-08-08 15:35:54 -0500

    First Attempt at CoinSorting project
    Added values for varables, allowed the computer to recieve coins,  convered coins to dollars

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
-var testing = "Hellu World"
-console.log(testing)
+//Step One. Assign Values
+
+const value_pennies = .01
+const value_nickles = .05
+const value_dimes = .10
+const value_quarters = .25
+//From the world of blackness i *the computer/page* emurge. Now knowing the value of pennies - quarters. 
+
+//Step Two. Calculate Total Values 
+
+const value_dollar = 1
+// from the blackness of the void i now understand a dollar is equal to 1
+//from the void i formally request an action i can do with my new knoweldge
+
+let PenniesRecieved = 120; 
+//pennies have sprinkled into my void. I am now aware that i can receive things. 
+
+let PenniesToDollars = (value_pennies * PenniesRecieved)
+console.log (PenniesToDollars)
+//from the void, i now grow aware that i can recieve pennies and that if i recieve an amount a new value will exist. 
+//from the void i grow more aware, pennies to dollars is just the value multiplied. I will apply this concept to my other coins. 
+
+let NicklesRecieved = 120
+
+let NicklesToDollars = (value_nickles * NicklesRecieved)
+console.log(NicklesToDollars)
+
+let DimesRecieved = 120
+
+let DimesToDollars = (value_nickles * DimesRecieved)
+console.log(DimesToDollars)
+
+let QuartersRecieved = 120
+
+let QuartersToDollars = (value_quarters * QuartersRecieved)
+console.log(QuartersToDollars)
+
+//From my void i grow futher aware... these coins * the values make dollars. I now have some dollars. 
+
+let Pennies_Wrapper = (value_pennies * 50)
+let Nickles_Wrapper = (value_pennies * 40)
+let Dimes_Wrapper = (value_dimes * 50)
+let Quarters_Wrapper = (value_quarters * 40) 
+
+console.log(Pennies_Wrapper, Nickles_Wrapper, Dimes_Wrapper, Quarters_Wrapper)
