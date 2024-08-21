aeaa098f7970d6e7ed282b048f7e3b436b85868c
K-D-Piper <kirsten.piper@mscoding.org>
2024-08-20 20:05:23 -0500

    Added Instructions. Finished First 2 instructions
    Added comments. Began working out the first. Added values to variables. Calculated values in dollars.

--- app.js
+++ app.js
@@ -26,61 +26,30 @@ Instructions:
 After finished go to discussion tab and submit link to the Javascript Coin Project.
 */
 
-//Hello Me! Lets see what we got today!
-    //coin roller. One must...
-        //1) know what is had. i.e. what is a coin?
-            //What is a penny, nickel, dime, quarter
-        //2) know how many coins equal a dollar. 
-            //So  1. What is a dollar? 
-            //and 2. How many coins are in a dollar.
-            //and 3. How do we show/teach the computer that these coins can be made into a dollar
-            //and 4. How many dollars do we have in coins in the end 
-                //oaky so this was a lot of words. To simplfy. heres whats being looked for.   
-                    //how many of each coin we got, how much is that, how many dollars we got based on the amount of coins had. 
-        //3) put coins into wrappers. 
-            //how many wrappers will one need based on the amount of coins had? How many will be outside the wrapper?
-        //4) kk. So. Now. Display
-            //1. Number of wrappers needed for each kind of coin 
-            //2. total in dollars of each kind of coin, indivuallly then combined. 
 
-/*
-    kk. So... hi
-    heres kinda how im picturing right now. Heres what i'm thinking heres what i want. 
-
-    i picture a lot of coins. Just a big table full of coins that i'm going to have to expain to my computer what they are, what they do, and how to count them. 
-    so first. how do i exaplin what they are. 
-*/
-
-//What is a ...__... section  (STEP ONE)
-
-//How many of a ...___... equal a dollar? (STEP TWO)
-
-//How many dollars do i have? In...___.... (STEP TWO AND A HALF)
-
-//How many coins to i have? How many wrappers will i need per coin type? How many will be left over (STEP THREE)
-
-//Gemmie results (STEP FOUR)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- //Challenge Data to test project. 
 var challengeData = ['dime','dime','dime','penny','nickel','penny','dime','quarter','dime','nickel','nickel','dime','quarter','nickel','penny','dime','nickel','dime','dime','dime','quarter','nickel','quarter','dime','nickel','penny','dime','penny','nickel','quarter','quarter','quarter','dime','nickel','dime','quarter','dime','penny','penny','nickel','penny','penny','dime','nickel','penny','penny','quarter','quarter','nickel','nickel','penny','dime','nickel','quarter','nickel','nickel','quarter','quarter','dime','nickel','penny','dime','dime','nickel','penny','quarter','quarter','quarter','dime','nickel','nickel','dime','quarter','quarter','nickel','penny','quarter','penny','quarter','nickel','dime','quarter','dime','penny','quarter','nickel','nickel','nickel','quarter','penny','nickel','nickel','dime','dime','dime','quarter','quarter','quarter','penny','penny','dime','dime','dime','penny','quarter','nickel','quarter','quarter','penny','nickel','penny','nickel','nickel','penny','quarter','nickel','dime','quarter','nickel','dime','penny','nickel','nickel','penny','penny','penny','dime','quarter','quarter','quarter','penny','dime','nickel','dime','dime','penny','quarter','penny','quarter','penny','dime','dime','nickel','dime','penny','penny','nickel','penny','nickel','quarter','dime','dime','nickel','nickel','nickel','dime','dime','dime','quarter','penny','dime','nickel','quarter','nickel','penny','nickel','penny','nickel','penny','nickel','nickel','penny','dime','nickel','dime','dime','quarter','quarter','quarter','penny','dime','penny','dime','dime','penny','penny','nickel','nickel','penny','quarter','nickel','quarter','dime','nickel','quarter','nickel','penny','dime','quarter','nickel','penny','quarter','quarter','penny','quarter','dime','penny','dime','nickel','penny','dime','quarter','nickel','quarter','quarter','quarter','dime','quarter','dime','quarter','nickel','penny','penny','quarter','quarter','penny','dime','penny','dime','penny','nickel','nickel','dime','penny','quarter','penny','quarter','nickel','quarter','penny','nickel','quarter','dime','quarter','dime','penny','dime','dime','quarter','penny','penny','nickel','nickel','quarter','nickel','quarter','dime','quarter','penny','penny','penny','nickel','nickel','quarter','penny','quarter','quarter','penny','nickel','dime','penny','quarter','nickel','dime','penny','penny','quarter','penny','penny','dime','nickel','dime','penny','dime','nickel','nickel','dime','dime','penny','nickel','penny','dime','dime','nickel','dime','dime','penny','penny','dime','penny','nickel','quarter','nickel','quarter','quarter','penny','nickel','penny','quarter','nickel','nickel','quarter','penny','penny','nickel','nickel','penny','penny','nickel','quarter','nickel','dime','nickel','nickel','penny','quarter','dime','dime','nickel','dime','nickel','nickel','dime','quarter','quarter','nickel','penny','dime','dime','dime','dime','quarter','nickel','nickel','dime','nickel','quarter','nickel','dime','quarter','nickel','dime','quarter','quarter','penny','quarter','nickel','dime','quarter','penny','penny','nickel','nickel','quarter','dime','penny','dime','dime','quarter','dime','penny','quarter','penny','penny','nickel','nickel','nickel','penny','penny','quarter','penny','penny','quarter','dime','quarter','nickel','quarter','nickel','penny','penny','penny','dime','dime','dime','dime','nickel','dime','quarter','nickel','dime','nickel','dime','dime','penny','dime','quarter','nickel','penny','nickel','penny','penny','quarter','dime','nickel','nickel','nickel','penny','nickel','dime','penny','quarter','quarter','dime','quarter','penny','nickel','nickel','penny','dime','dime','nickel','nickel','dime','penny','nickel','penny','quarter','dime','nickel','dime','penny','penny','quarter','dime','quarter','penny','penny','nickel','quarter','quarter','quarter','dime','penny','dime','dime','dime','quarter','penny','penny','dime','quarter','dime','penny','quarter','dime','dime','dime']
+
+//STEP ONE : DEFINE VARIABLE FOR COINS 
+//STEP TWO : CALCULATE VALUES FOR COINS IN DOLLARS/USD
+let penny = ['penny', .01] // Define penny
+let PennyCount = challengeData.filter(element => {return element === 'penny'}).length //How many pennies do you have? //117
+console.log(`There are ${PennyCount} pennies! ` + ` That equals to $ ${(PennyCount * .01).toFixed(2)}`) // How many pennies are in this dollar? //117 ; $1.17
+
+let nickel = ['nickel', .05] //Define nickel 
+let NickelCount = challengeData.filter(element => {return element === 'nickel'}).length // How many nickels do you have? //118 
+console.log(`There are ${NickelCount} nickels! ` + ` That equals to $ ${(NickelCount * .05).toFixed(2)}`) //How many nickels are in a dollar? //118 ; $ 5.90
+
+let dime = ['dime', .10] //Define dime
+let DimeCount = challengeData.filter(element => {return element === 'dime'}).length //How many dimes do you have? //123
+console.log(`There are ${DimeCount} dimes! ` + ` That equals to $ ${(DimeCount * .10).toFixed(2)}`) //123 ; $12.30
+
+let quarter = ['quarter', .25] //Define Quarter
+let QuarterCount = challengeData.filter(element => {return element === 'quarter'}).length //How many Quarters do you have //109
+console.log(`There are ${QuarterCount} quarters! ` + ` That equals to $ ${(QuarterCount * .25).toFixed(2)}`) //How many quarters are in a dollar? //109 ; $27.25
+
+let coins = challengeData.length
+let TotalCount = (PennyCount * .01) + (NickelCount * .05) + (DimeCount * .10) + (QuarterCount * .25) //What is the final total? $ 46.62
+console.log(`There are ${coins} coins! Thats $ ${TotalCount.toFixed(2)} in total!`) 
 
 
 /* First Attempt. Commented out but still may use for refernecne. 
@@ -134,7 +103,6 @@ console.log(Pennies_Wrapper, Nickles_Wrapper, Dimes_Wrapper, Quarters_Wrapper)
 
 // QUIZ 8/9/2024
 
-var challengeData = ['dime','dime','dime','penny','nickel','penny','dime','quarter','dime','nickel','nickel','dime','quarter','nickel','penny','dime','nickel','dime','dime','dime','quarter','nickel','quarter','dime','nickel','penny','dime','penny','nickel','quarter','quarter','quarter','dime','nickel','dime','quarter','dime','penny','penny','nickel','penny','penny','dime','nickel','penny','penny','quarter','quarter','nickel','nickel','penny','dime','nickel','quarter','nickel','nickel','quarter','quarter','dime','nickel','penny','dime','dime','nickel','penny','quarter','quarter','quarter','dime','nickel','nickel','dime','quarter','quarter','nickel','penny','quarter','penny','quarter','nickel','dime','quarter','dime','penny','quarter','nickel','nickel','nickel','quarter','penny','nickel','nickel','dime','dime','dime','quarter','quarter','quarter','penny','penny','dime','dime','dime','penny','quarter','nickel','quarter','quarter','penny','nickel','penny','nickel','nickel','penny','quarter','nickel','dime','quarter','nickel','dime','penny','nickel','nickel','penny','penny','penny','dime','quarter','quarter','quarter','penny','dime','nickel','dime','dime','penny','quarter','penny','quarter','penny','dime','dime','nickel','dime','penny','penny','nickel','penny','nickel','quarter','dime','dime','nickel','nickel','nickel','dime','dime','dime','quarter','penny','dime','nickel','quarter','nickel','penny','nickel','penny','nickel','penny','nickel','nickel','penny','dime','nickel','dime','dime','quarter','quarter','quarter','penny','dime','penny','dime','dime','penny','penny','nickel','nickel','penny','quarter','nickel','quarter','dime','nickel','quarter','nickel','penny','dime','quarter','nickel','penny','quarter','quarter','penny','quarter','dime','penny','dime','nickel','penny','dime','quarter','nickel','quarter','quarter','quarter','dime','quarter','dime','quarter','nickel','penny','penny','quarter','quarter','penny','dime','penny','dime','penny','nickel','nickel','dime','penny','quarter','penny','quarter','nickel','quarter','penny','nickel','quarter','dime','quarter','dime','penny','dime','dime','quarter','penny','penny','nickel','nickel','quarter','nickel','quarter','dime','quarter','penny','penny','penny','nickel','nickel','quarter','penny','quarter','quarter','penny','nickel','dime','penny','quarter','nickel','dime','penny','penny','quarter','penny','penny','dime','nickel','dime','penny','dime','nickel','nickel','dime','dime','penny','nickel','penny','dime','dime','nickel','dime','dime','penny','penny','dime','penny','nickel','quarter','nickel','quarter','quarter','penny','nickel','penny','quarter','nickel','nickel','quarter','penny','penny','nickel','nickel','penny','penny','nickel','quarter','nickel','dime','nickel','nickel','penny','quarter','dime','dime','nickel','dime','nickel','nickel','dime','quarter','quarter','nickel','penny','dime','dime','dime','dime','quarter','nickel','nickel','dime','nickel','quarter','nickel','dime','quarter','nickel','dime','quarter','quarter','penny','quarter','nickel','dime','quarter','penny','penny','nickel','nickel','quarter','dime','penny','dime','dime','quarter','dime','penny','quarter','penny','penny','nickel','nickel','nickel','penny','penny','quarter','penny','penny','quarter','dime','quarter','nickel','quarter','nickel','penny','penny','penny','dime','dime','dime','dime','nickel','dime','quarter','nickel','dime','nickel','dime','dime','penny','dime','quarter','nickel','penny','nickel','penny','penny','quarter','dime','nickel','nickel','nickel','penny','nickel','dime','penny','quarter','quarter','dime','quarter','penny','nickel','nickel','penny','dime','dime','nickel','nickel','dime','penny','nickel','penny','quarter','dime','nickel','dime','penny','penny','quarter','dime','quarter','penny','penny','nickel','quarter','quarter','quarter','dime','penny','dime','dime','dime','quarter','penny','penny','dime','quarter','dime','penny','quarter','dime','dime','dime']
 
 //? Give me the total amount of each coin;
 // ex. dime = 234;
@@ -204,4 +172,120 @@ for( let i=0; i < challengeData.length; i++) {
     //
     if()
 }
+*/
+
+/*
+Coin Sorter and Wrapper Calculator
+Project: Coin Sorter and Wrapper Calculator
+Objective:
+Create a JavaScript program that calculates the total value of coins in US currency (pennies, nickels, dimes, and quarters) and sorts them into coin wrappers. The program will then output the number of coin wrappers needed and the total amount in dollars.
+
+Instructions:
+1 . Define Variables for Coins:
+    Create variables to store the number of each type of coin: pennies, nickels, dimes, and quarters.
+
+2. Calculate Total Value of Coins:
+    Write code to calculate the total value of each type of coin in dollars.
+    Calculate the combined total value of all coins.
+3. Sort Coins into Wrappers:
+    Use the standard capacity for each type of coin wrapper:
+        Pennies: 50 coins per wrapper
+        Nickels: 40 coins per wrapper
+        Dimes: 50 coins per wrapper
+        Quarters: 40 coins per wrapper
+    Calculate the number of wrappers needed for each type of coin.
+    Calculate the remaining coins that do not fill a wrapper completely.
+4. Output Results:
+    Use console.log to display the number of wrappers needed for each type of coin and remaining coins that do not fill a wrapper
+    Use console.log to display the total amount in dollars.
+
+After finished go to discussion tab and submit link to the Javascript Coin Project.
+*/
+
+//Hello Me! Lets see what we got today!
+    //coin roller. One must...
+        //1) know what is had. i.e. what is a coin?
+            //What is a penny, nickel, dime, quarter
+        //2) know how many coins equal a dollar. 
+            //So  1. What is a dollar? 
+            //and 2. How many coins are in a dollar.
+            //and 3. How do we show/teach the computer that these coins can be made into a dollar
+            //and 4. How many dollars do we have in coins in the end 
+                //oaky so this was a lot of words. To simplfy. heres whats being looked for.   
+                    //how many of each coin we got, how much is that, how many dollars we got based on the amount of coins had. 
+        //3) put coins into wrappers. 
+            //how many wrappers will one need based on the amount of coins had? How many will be outside the wrapper?
+        //4) kk. So. Now. Display
+            //1. Number of wrappers needed for each kind of coin 
+            //2. total in dollars of each kind of coin, indivuallly then combined. 
+
+/*
+    kk. So... hi
+    heres kinda how im picturing right now. Heres what i'm thinking heres what i want. 
+
+    i picture a lot of coins. Just a big table full of coins that i'm going to have to expain to my computer what they are, what they do, and how to count them. 
+    so first. how do i exaplin what they are. 
+*/
+
+//What is a ...__... section  (STEP ONE)
+
+//How many of a ...___... equal a dollar? (STEP TWO)
+
+//How many dollars do i have? In...___.... (STEP TWO AND A HALF)
+
+//How many coins to i have? How many wrappers will i need per coin type? How many will be left over (STEP THREE)
+
+//Gemmie results (STEP FOUR)
+
+
+//Thought / Goal.2  Nested For loops. First one is to sort (and hopefully convert)
+//Swich or if statement? 
+
+//for loop 1. The outside one. Needs to just count. 
+
+//STEP ONE defining things
+//potental options 
+/*
+let penny = 'penny'
+
+
+*/
+/*
+let penny = ['penny' , .01]
+
+console.log(PennyCount) //=117
+*/
+
+// {
+           //     console.log(`There are ${PennyCount} pennies! ` + `That equals to $ ${(PennyCount * .01).toFixed(2)}`)
+          //  }
+/*
+
+for (coin = 0 ; coin < challengeData.length ; coin ++)
+    {
+        console.log(`There is ${challengeData.length} coins`) //i like. keep. 
+            //of the 467 coins there are... 
+    }
+        if ('penny') 
+            {
+            console.log(`There are ${PennyCount} pennies! ` + `That equals to $ ${(PennyCount * .01).toFixed(2)}`)
+            } else if ('dime'){
+            console.log(`There are ${DimeCount} dimes! ` + `That equals to $ ${(DimeCount * .10).toFixed(2)}`)
+           } else if ('nickel')
+           {
+            console.log(`There are ${NickelCount} dimes! ` + `That equals to $ ${(NickelCount * .05).toFixed(2)}`)
+           }
+
+*/
+
+/*
+my potential nested for loop outside. / goal adn desire
+
+for (coin = 0 ; coin < challengeData.length ; coin ++){
+    console.log ( There are &{ this many} coins )
+
+        for (pen = 0; pen < challenge.Data.lenght ; pen++)
+
+}
+
 */
